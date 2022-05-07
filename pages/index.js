f6331dfe80ba530a036a58f88ccbd9000e057355
SidathDabare <sidath2007@gmail.com>
2022-05-07 23:28:41 +0200

    corrections

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,41 @@
+//show search bar---------------------
+let searchBar = document.querySelector("#searchBar");
+function showSearch() {
+  searchBar.classList.add("input-display");
+}
+// Change navbar bg---------------------
+window.onscroll = function () {
+  scrollNavbar();
+};
+function scrollNavbar() {
+  let navbarBg = document.querySelector("#navbarBg");
+  let navLinks = document.querySelectorAll(".nav-link");
+  console.log(navLinks);
+  if (document.documentElement.scrollTop > 2) {
+    navbarBg.classList.add("scroll");
+    // Change the color of navLinks on scroll
+    for (let i = 0; i < navLinks.length; i++) {
+      const element = navLinks[i];
+      element.classList.add(".linkColor");
+    }
+  } else {
+    navbarBg.classList.remove("scroll");
+    // Change the color of navLinks back to default
+    for (let i = 0; i < navLinks.length; i++) {
+      const element = navLinks[i];
+      element.classList.remove(".linkColor");
+    }
+  }
+}
+//------------------------------------------------------------
+const slider = document.querySelector(".slider");
+const btnLeft = document.getElementById("moveLeft");
+const btnRight = document.getElementById("moveRight");
+const indicators = document.querySelectorAll(".indicator");
+
+let baseSliderWidth = slider.offsetWidth;
+let activeIndex = 0; // the current page on the slider
+
 let movies = [
   {
     src: "https://images.unsplash.com/photo-1585951237318-9ea5e175b891?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
@@ -57,3 +95,86 @@ let movies = [
     src: "https://images.unsplash.com/photo-1536300007881-7e482242baa5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
   },
 ];
+
+// Fill the slider with all the movies in the "movies" array
+function populateSlider() {
+  movies.forEach((image) => {
+    // Clone the initial movie thats included in the html, then replace the image with a different one
+    const newMovie = document.getElementById("movie0");
+    let clone = newMovie.cloneNode(true);
+    let img = clone.querySelector("img");
+    img.src = image.src;
+
+    slider.insertBefore(clone, slider.childNodes[slider.childNodes.length - 1]);
+  });
+}
+
+populateSlider();
+
+// delete the initial movie in the html
+const initialMovie = document.getElementById("movie0");
+initialMovie.remove();
+
+// Update the indicators that show which page we're currently on
+function updateIndicators(index) {
+  indicators.forEach((indicator) => {
+    indicator.classList.remove("current");
+  });
+  let newActiveIndicator = indicators[index];
+  newActiveIndicator.classList.add("current");
+}
+
+// Scroll Left button
+btnLeft.addEventListener("click", (e) => {
+  let movieWidth = document
+    .querySelector(".movie")
+    .getBoundingClientRect().width;
+  let scrollDistance = movieWidth * 6; // Scroll the length of 6 movies. TODO: make work for mobile because (4 movies/page instead of 6)
+
+  slider.scrollBy({
+    top: 0,
+    left: -scrollDistance,
+    behavior: "smooth",
+  });
+  activeIndex = (activeIndex - 1) % 3;
+  console.log(activeIndex);
+  updateIndicators(activeIndex);
+});
+
+// Scroll Right button
+btnRight.addEventListener("click", (e) => {
+  let movieWidth = document
+    .querySelector(".movie")
+    .getBoundingClientRect().width;
+  let scrollDistance = movieWidth * 6; // Scroll the length of 6 movies. TODO: make work for mobile because (4 movies/page instead of 6)
+
+  console.log(`movieWidth = ${movieWidth}`);
+  console.log(`scrolling right ${scrollDistance}`);
+
+  // if we're on the last page
+  if (activeIndex == 2) {
+    // duplicate all the items in the slider (this is how we make 'looping' slider)
+    populateSlider();
+    slider.scrollBy({
+      top: 0,
+      left: +scrollDistance,
+      behavior: "smooth",
+    });
+    activeIndex = 0;
+    updateIndicators(activeIndex);
+  } else {
+    slider.scrollBy({
+      top: 0,
+      left: +scrollDistance,
+      behavior: "smooth",
+    });
+    activeIndex = (activeIndex + 1) % 3;
+    console.log(activeIndex);
+    updateIndicators(activeIndex);
+  }
+});
+
+// slider.addEventListener("scroll", (e) => {
+//   console.log(slider.scrollLeft);
+//   console.log(slider.offsetWidth);
+// });
