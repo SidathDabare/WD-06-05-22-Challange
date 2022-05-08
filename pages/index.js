ab4234c2b91b5cfc959a7f941141b62653b49889
SidathDabare <sidath2007@gmail.com>
2022-05-08 22:43:12 +0200

    corrections

--- pages/index.js
+++ pages/index.js
@@ -38,61 +38,67 @@ let activeIndex = 0; // the current page on the slider
 
 let movies = [
   {
-    src: "https://images.unsplash.com/photo-1585951237318-9ea5e175b891?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    src: "./assets/video-bg.mp4",
   },
   {
-    src: "https://images.unsplash.com/photo-1579566346927-c68383817a25?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-  },
-
-  {
-    src: "https://images.unsplash.com/photo-1594736797933-d0501ba2fe65?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=674&q=80",
-  },
-
-  {
-    src: "https://images.unsplash.com/photo-1617182635496-c5c474367085?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+    src: "./assets/blossoms_-_113004 (Original).mp4",
   },
   {
-    src: "https://images.unsplash.com/photo-1611419010196-a360856fc42f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+    src: "./assets/trees_-_111973 (Original).mp4",
   },
   {
-    src: "https://images.unsplash.com/photo-1528495612343-9ca9f4a4de28?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
+    src: "./assets/video-bg.mp4",
   },
   {
-    src: "https://images.unsplash.com/photo-1518715303843-586e350765b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    src: "./assets/blossoms_-_113004 (Original).mp4",
   },
   {
-    src: "https://images.unsplash.com/photo-1617258683488-df59909f25f0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=644&q=80",
+    src: "./assets/trees_-_111973 (Original).mp4",
   },
   {
-    src: "https://images.unsplash.com/photo-1543862809-2c9e0bcdc075?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=564&q=80",
+    src: "./assets/video-bg.mp4",
   },
   {
-    src: "https://images.unsplash.com/photo-1579156412503-f22426cc6386?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1352&q=80",
+    src: "./assets/blossoms_-_113004 (Original).mp4",
   },
   {
-    src: "https://images.unsplash.com/photo-1514068574489-503a8eb91592?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1390&q=80",
+    src: "./assets/trees_-_111973 (Original).mp4",
   },
   {
-    src: "https://images.unsplash.com/photo-1521714161819-15534968fc5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    src: "./assets/video-bg.mp4",
   },
   {
-    src: "https://images.unsplash.com/photo-1572188863110-46d457c9234d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    src: "./assets/blossoms_-_113004 (Original).mp4",
   },
   {
-    src: "https://images.unsplash.com/photo-1579702455224-c0dd4ac78234?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1369&q=80",
-  },
-
-  {
-    src: "https://images.unsplash.com/photo-1575470180257-7183ddca844f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80",
+    src: "./assets/trees_-_111973 (Original).mp4",
   },
   {
-    src: "https://images.unsplash.com/photo-1584253660192-de72b033c220?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+    src: "./assets/video-bg.mp4",
   },
   {
-    src: "https://images.unsplash.com/photo-1611523792722-16952e48cffa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+    src: "./assets/blossoms_-_113004 (Original).mp4",
   },
   {
-    src: "https://images.unsplash.com/photo-1536300007881-7e482242baa5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    src: "./assets/trees_-_111973 (Original).mp4",
+  },
+  {
+    src: "./assets/video-bg.mp4",
+  },
+  {
+    src: "./assets/blossoms_-_113004 (Original).mp4",
+  },
+  {
+    src: "./assets/trees_-_111973 (Original).mp4",
+  },
+  {
+    src: "./assets/video-bg.mp4",
+  },
+  {
+    src: "./assets/blossoms_-_113004 (Original).mp4",
+  },
+  {
+    src: "./assets/trees_-_111973 (Original).mp4",
   },
 ];
 
@@ -102,7 +108,7 @@ function populateSlider() {
     // Clone the initial movie thats included in the html, then replace the image with a different one
     const newMovie = document.getElementById("movie0");
     let clone = newMovie.cloneNode(true);
-    let img = clone.querySelector("img");
+    let img = clone.querySelector("video");
     img.src = image.src;
 
     slider.insertBefore(clone, slider.childNodes[slider.childNodes.length - 1]);
@@ -110,7 +116,14 @@ function populateSlider() {
 }
 
 populateSlider();
-
+// play video on hover---------------------
+const VideoController = function (isHovering, videoElement) {
+  if (isHovering == true) {
+    videoElement.play();
+  } else if (isHovering == false) {
+    videoElement.pause();
+  }
+};
 // delete the initial movie in the html
 const initialMovie = document.getElementById("movie0");
 initialMovie.remove();
